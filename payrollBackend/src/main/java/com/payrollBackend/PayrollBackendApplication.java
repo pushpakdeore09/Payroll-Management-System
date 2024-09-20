87c2aec8801cac9f8fa21db6600399873743eba4
pushpakdeore09 <pushpakdeore9114@gmail.com>
2024-09-21 01:45:40 +0530

    first commit

---
+++ payrollBackend/src/main/java/com/payrollBackend/PayrollBackendApplication.java
@@ -0,0 +1,13 @@
+package com.payrollBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollBackendApplication.class, args);
+	}
+
+}
---
+++ payrollBackend/src/test/java/com/payrollBackend/PayrollBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payrollBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
